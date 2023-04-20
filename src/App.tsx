39b83fb3240aaae1b6af4e3721f42671de71439c
Oleg <Oleg@DESKTOP-B5TOANR>
2023-04-20 17:23:36 +0300

    Printing ability

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [wordsState, setWordsState] = useState(new Array(30).fill(""));
+
+    useEffect(() => {
+        document.onkeyup = (event) => {
+            setWordsState((prevState) => {
+                let isSetted = false;
+
+                return prevState.map((val, index) => {
+                    if (!isSetted && event.key === "Backspace" && val && !prevState[index + 1]) {
+                        isSetted = true;
+                        return "";
+                    }
+
+                    if (!isSetted && !val && event.key.length === 1) {
+                        isSetted = true;
+                        return event.key;
+                    }
+
+                    return val;
+                })
+            })
+        }
+
+    }, []);
+
+    return (
+        <div className="app">
+            <div className="grid">
+                {wordsState.map(word => <div className="cell">{word}</div>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
