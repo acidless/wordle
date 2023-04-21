6ca75226a2be084bf9cefde82c38eda8663001e2
Oleg <Oleg@DESKTOP-B5TOANR>
2023-04-21 21:14:35 +0300

    Added simple word check

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './App.css';
 
 const WORD_LENGTH = 5;
@@ -22,13 +22,12 @@ function App() {
 
     const [currentWord, setCurrentWord] = useState("PILOT");
     const [currentTry, setCurrentTry] = useState(0);
+    const onKeyUp = useCallback((event:any) => {
+        setLetterState((prevState) => {
+            let isSetted = false;
 
-    useEffect(() => {
-        document.onkeyup = (event) => {
-            setLetterState((prevState) => {
-                let isSetted = false;
-
-                return prevState.map((val, index) => {
+            return prevState.map((val, index) => {
+                if (index >= WORD_LENGTH * currentTry && index < WORD_LENGTH * (currentTry + 1)) {
                     if (!isSetted && event.key === "Backspace" && val && !prevState[index + 1]) {
                         isSetted = true;
                         return "";
@@ -38,32 +37,40 @@ function App() {
                         isSetted = true;
                         return event.key.toUpperCase();
                     }
-
-                    return val;
-                })
-            });
-        }
-
-    }, []);
-
-    useEffect(() => {
-        let counter = 0;
-        for (let i = WORD_LENGTH * currentTry; i < WORD_LENGTH; i++) {
-            if (letterState[i]) {
-                let state = "b";
-                if (currentWord[counter] === letterState[i]) {
-                    state = "g";
-                } else if (currentWord.includes(letterState[i])) {
-                    state = "y";
                 }
 
-                setCorrectState((prevState) => prevState.map((val, index) => index === i ? state : val))
+                return val;
+            })
+        });
+    }, [currentTry]);
+
+    useEffect(() => {
+        document.onkeyup = onKeyUp;
+
+    }, [onKeyUp]);
+
+    function checkLetters() {
+        if (letterState[WORD_LENGTH * (currentTry + 1) - 1]) {
+            let counter = 0;
+            for (let i = WORD_LENGTH * currentTry; i < WORD_LENGTH * (currentTry + 1); i++) {
+                if (letterState[i]) {
+                    let state = "b";
+                    if (currentWord[counter] === letterState[i]) {
+                        state = "g";
+                    } else if (currentWord.includes(letterState[i])) {
+                        state = "y";
+                    }
+
+                    setCorrectState((prevState) => prevState.map((val, index) => index === i ? state : val))
+                }
+
+
+                counter++;
             }
 
-
-            counter++;
+            setCurrentTry(prevState => prevState + 1);
         }
-    }, [letterState]);
+    }
 
     return (
         <div className="app">
@@ -71,6 +78,7 @@ function App() {
                 {letterState.map((letter, index) => <div className="cell"
                                                          style={{backgroundColor: getColor(correctState[index])}}>{letter}</div>)}
             </div>
+            <button onClick={checkLetters}>Submit</button>
         </div>
     );
 }
